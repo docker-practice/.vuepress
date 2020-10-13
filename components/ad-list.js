51787568e5863c5fb1d9f7669344da42330233a5
Kang Huaishuai <khs1994@khs1994.com>
2020-10-13 22:23:19 +0800

    init

---
+++ components/ad-list.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: '腾讯云',
+    description: '【腾讯云】星星海 SA2 云服务器。',
+    strong: '1 核 2G 首年 99 元起，高性价比首选',
+    action: '立即抢购',
+    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1063&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+    weight: 100
+  },
+  {
+    name: '腾讯云',
+    description: '【腾讯云】云产品限时秒杀。',
+    strong: '1 核 2G 云服务器，首年 99 元',
+    action: '立即抢购',
+    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+    weight: 100
+  },
+  {
+    name: '阿里云',
+    description: '云服务器 精选特惠，',
+    strong: '低至 0.95 折起',
+    action: '去抢',
+    url: 'https://www.aliyun.com/product/ecs?source=5176.11533457&userCode=8lx5zmtu',
+    weight: 100
+  },
+]
