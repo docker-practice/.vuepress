72095c319ad49ced53e8c13d5994ac21609787f6
Kang Huaishuai <khs1994@khs1994.com>
2020-11-02 10:30:33 +0800

    Update ad-list.js

--- components/ad-list.js
+++ components/ad-list.js
@@ -1,34 +1,45 @@
 module.exports = [
+//   {
+//     name: '腾讯云',
+//     description: '【腾讯云】星星海 SA2 云服务器。',
+//     strong: '1 核 2G 首年 99 元起，高性价比首选',
+//     action: '立即抢购',
+//     url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1063&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+//     weight: 25
+//   },
   {
     name: '腾讯云',
-    description: '【腾讯云】星星海 SA2 云服务器。',
-    strong: '1 核 2G 首年 99 元起，高性价比首选',
+    description: '【腾讯云】双 11 云上盛惠。【秒杀】【满返】【抽奖】',
+    strong: '1 核 2G 云服务器，首年 88 元',
     action: '立即抢购',
-    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1063&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
-    weight: 25
+    //url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=33867&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=activity',
+    weight: 95
   },
-  {
-    name: '腾讯云',
-    description: '【腾讯云】云产品限时秒杀。',
-    strong: '1 核 2G 云服务器，首年 99 元',
-    action: '立即抢购',
-    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
-    weight: 50
-  },
+//   {
+//     name: '阿里云',
+//     description: '云服务器 精选特惠，',
+//     strong: '低至 0.73 折起',
+//     action: '去抢',
+//     //url: 'https://www.aliyun.com/product/ecs?source=5176.11533457&userCode=8lx5zmtu',
+//     url: 'https://www.aliyun.com/1111/pintuan-share?ptCode=MTM4MjEzNzgzMTg0MTgzOXx8MTE0fDE%3D&userCode=8lx5zmtu',
+//     weight: 100
+//   },
   {
     name: '阿里云',
-    description: '云服务器 精选特惠，',
-    strong: '低至 0.95 折起',
+    description: '双 11 上云狂欢节，【 开宝箱，抽红包 】',
+    strong: '云服务器 【 84.97/年 】起',
     action: '去抢',
-    url: 'https://www.aliyun.com/product/ecs?source=5176.11533457&userCode=8lx5zmtu',
-    weight: 25
-  },
-  {
-    name: '加入微信群聊',
-    description: '与 1100+ Docker 使用者交流',
-    strong: '',
-    action: '点击加入',
-    url: 'https://yewm28.coding-pages.com/wechat.jpg',
-    weight: 25
+    //url: 'https://www.aliyun.com/product/ecs?source=5176.11533457&userCode=8lx5zmtu',
+    url: 'https://www.aliyun.com/1111/pintuan-share?ptCode=MTM4MjEzNzgzMTg0MTgzOXx8MTE0fDE%3D&userCode=8lx5zmtu',
+    weight: 100
   },  
+//   {
+//     name: '加入微信群聊',
+//     description: '与 1100+ Docker 使用者交流',
+//     strong: '',
+//     action: '点击加入',
+//     url: 'https://yewm28.coding-pages.com/wechat.jpg',
+//     weight: 25
+//   },  
 ]
