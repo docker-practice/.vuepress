aba4e3d0460e50518de7e722a693053980f5842a
Kang Huaishuai <khs1994@khs1994.com>
2020-12-10 00:15:27 +0800

    Update ad-list.js

--- components/ad-list.js
+++ components/ad-list.js
@@ -1,19 +1,20 @@
 module.exports = [
-//   {
-//     name: '腾讯云',
-//     description: '【腾讯云】星星海 SA2 云服务器。',
-//     strong: '1 核 2G 首年 99 元起，高性价比首选',
-//     action: '立即抢购',
-//     url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1063&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
-//     weight: 25
-//   },
   {
     name: '腾讯云',
-    description: '【腾讯云】双 11 云上盛惠。【秒杀】【满返】【抽奖】',
-    strong: '1 核 2G 云服务器，首年 88 元',
+    description: '【腾讯云】星星海 SA2 云服务器。',
+    strong: '1 核 2G 首年 99 元起，高性价比首选',
+    action: '立即抢购',
+    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1063&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+    weight: 95
+  },
+  {
+    name: '腾讯云',
+    description: '',
+    strong: '1 核 2G 云服务器，首年 99 元',
     action: '立即抢购',
     //url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
-    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=33867&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=activity',
+    // url: 'https://cloud.tencent.com/act/cps/redirect?redirect=33867&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=activity',
+    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
     weight: 95
   },
   {
@@ -26,12 +27,13 @@ module.exports = [
   },
   {
     name: '阿里云',
-    description: '双 11 上云狂欢节，【 开宝箱，抽红包 】',
-    strong: '云服务器 【 84.97/年 】起',
-    action: '去抢',
+    description: '双 12 爆款特惠',
+    strong: '',
+    action: '去看看',
     //url: 'https://www.aliyun.com/product/ecs?source=5176.11533457&userCode=8lx5zmtu',
-    url: 'https://www.aliyun.com/1111/pintuan-share?ptCode=MTM4MjEzNzgzMTg0MTgzOXx8MTE0fDE%3D&userCode=8lx5zmtu',
-    weight: 100
+    //url: 'https://www.aliyun.com/1111/pintuan-share?ptCode=MTM4MjEzNzgzMTg0MTgzOXx8MTE0fDE%3D&userCode=8lx5zmtu',
+    url: 'https://www.aliyun.com/minisite/goods?taskCode=pintuan20201212&recordId=296377&userCode=8lx5zmtu',
+    weight: 95
   },  
 //   {
 //     name: '加入微信群聊',
