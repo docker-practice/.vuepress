6f0532d022fe84ece9535fa14e49a59e38bf195c
Kang Huaishuai <khs1994@khs1994.com>
2021-05-31 19:15:51 +0800

    Update ad-list.js

--- components/ad-list.js
+++ components/ad-list.js
@@ -7,31 +7,32 @@ module.exports = [
 //     url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1063&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
 //     weight: 95
 //   },
-  {
-    name: '腾讯云',
-    description: '',
-    strong: '1 核 2G 云服务器，首年 99 元',
-    action: '立即抢购',
-    //url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
-    url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
-    weight: 95
-  },
-  {
-    name: '华为云',
-    description: '1 核 2G，云耀云服务器',
-    strong: '88元/年起',
-    action: '去抢',
-    url: 'https://activity.huaweicloud.com/1111_promotion/invite.html?fromacct=d4a5a111-c135-4c02-bc2d-3482d5faf2cc',
-    weight: 50
-  },
+//   {
+//     name: '腾讯云',
+//     description: '',
+//     strong: '1 核 2G 云服务器，首年 99 元',
+//     action: '立即抢购',
+//     //url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+//     url: 'https://cloud.tencent.com/act/cps/redirect?redirect=1062&cps_key=3a5255852d5db99dcd5da4c72f05df61&from=console',
+//     weight: 95
+//   },
+//   {
+//     name: '华为云',
+//     description: '1 核 2G，云耀云服务器',
+//     strong: '88元/年起',
+//     action: '去抢',
+//     url: 'https://activity.huaweicloud.com/1111_promotion/invite.html?fromacct=d4a5a111-c135-4c02-bc2d-3482d5faf2cc',
+//     weight: 50
+//   },
   {
     name: '阿里云',
-    description: '2021上云采购季',
-    strong: '2 核 2G 86元/年 更多云产品爆款特惠',
+    // description: '2021上云采购季',
+    description: '618 钜惠',
+    strong: '1 核 2G 79.86元/年 更多云产品爆款特惠',
     action: '去看看',
     //url: 'https://www.aliyun.com/product/ecs?source=5176.11533457&userCode=8lx5zmtu',
-    //url: 'https://www.aliyun.com/1111/pintuan-share?ptCode=MTM4MjEzNzgzMTg0MTgzOXx8MTE0fDE%3D&userCode=8lx5zmtu',
-    url: 'https://www.aliyun.com/minisite/goods?userCode=8lx5zmtu&share_source=copy_link',
+    url: 'https://www.aliyun.com/activity/ambassador/share-gift/goods?taskCode=share618&recordId=691752&userCode=8lx5zmtu',
+    // url: 'https://www.aliyun.com/minisite/goods?userCode=8lx5zmtu&share_source=copy_link',
     weight: 95
   },  
 //   {
